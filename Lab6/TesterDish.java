10d308e0d2ac979b19fd94c7e4bb4547c6cd0636
Bogdanovasofia <56391388+Bogdanovasofia@users.noreply.github.com>
2019-12-12 15:33:46 +0300

    Delete TesterDish.java

--- Lab6/TesterDish.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class TesterDish {
-    public static void main(String[] args) {
-        Tarelka t = new Tarelka("железная", "серая", "средняя");
-        System.out.println(t);
-    }
-}
