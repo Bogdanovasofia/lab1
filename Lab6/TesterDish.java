0704792da59b4b7999045006f0e89b16ccb7f9fd
Bogdanovasofia <56391388+Bogdanovasofia@users.noreply.github.com>
2019-12-12 15:22:54 +0300

    Add files via upload

---
+++ Lab6/TesterDish.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class TesterDish {
+    public static void main(String[] args) {
+        Tarelka t = new Tarelka("железная", "серая", "средняя");
+        System.out.println(t);
+    }
+}
