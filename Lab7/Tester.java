b37289517a02475eaf0658e818314a1afffcdd4c
Bogdanovasofia <56391388+Bogdanovasofia@users.noreply.github.com>
2019-12-12 15:39:49 +0300

    Add files via upload

---
+++ Lab7/Tester.java
@@ -0,0 +1,12 @@
+package Lab7;
+
+    public class Tester {
+        public static void main(String[] args) {
+            Car c1 = new Car("Audi", "S-klasse", "I123", 89000000, 2010);
+            c1.println();
+            Car c2 = new Car("BMW", "7 Series", "750 Li", 145699870, 2011);
+            c2.println();
+            Car c3 = new Car("Audi", "S1", "H", 36400000, 2012);
+            c3.println();
+        }
+}
